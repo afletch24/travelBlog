35c256202d78259c503b68e6689516bd7c43ade7
Audrey <anfletcher24@gmail.com>
2018-05-15 18:50:57 -0500

    added ability to filter photos by national park

---
+++ script.js
@@ -0,0 +1,25 @@
+let imgBlockArray = document.getElementsByClassName("imgBlock");
+const npBtn = document.getElementById("park");
+
+
+Array.from(imgBlockArray).forEach(block => {
+    block.addEventListener("click", function(event){
+        console.log("click")
+        console.log(block);
+    })
+})
+
+const filterForNP = () => {
+    for(i=0; i<imgBlockArray.length; i++){
+        let block = imgBlockArray[i];
+        let blockVal = imgBlockArray[i].getAttribute("value");
+        
+        if(blockVal === "np"){
+        }else{
+            block.classList.add("hide");
+        }
+    }
+};
+
+npBtn.addEventListener("click", filterForNP);
+
